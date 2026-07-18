62547be75e8ea9a17233a61b2c5381b77b5356a4
张兵 <1691635403@qq.com>
2026-07-18 16:54:51 +0800

    feature: 登录页面布局组件、登录组件开发等

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,113 @@
 <template>
-  <div>登录</div>
+  <div class="container">
+    <div class="title">
+      <div class="back-home">
+        <el-icon>
+          <Back />
+        </el-icon>
+        <span>返回首页</span>
+      </div>
+      <div class="title-text">
+        <h2>登录您的账户</h2>
+        <p>输入您的登录信息</p>
+      </div>
+    </div>
+    <div class="account-form">
+      <el-form
+        ref="formRef"
+        :model="formData"
+        :rules="formRules"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input
+            v-model="formData.username"
+            placeholder="请输入用户名"
+            size="large"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="formData.password"
+            placeholder="请输入密码"
+            type="password"
+            show-password
+            size="large"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div class="footer">
+        <el-button
+          class="login-btn"
+          type="primary"
+          size="large"
+          @click="handleLogin(formRef)"
+          >登录账户</el-button
+        >
+        <div class="register-btn">
+          <p>
+            还没有账户？<router-link to="/auth/register">去注册</router-link>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { ref, reactive } from "vue";
+
+const formRef = ref(null);
+const formData = reactive({
+  username: "",
+  password: "",
+});
+const formRules = reactive({
+  username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+  password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+});
+
+const handleLogin = async (formEl) => {
+  console.log("表单", formEl);
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    console.log("验证结果", valid, fields);
+    if (valid) {
+      console.log(fields);
+    }
+  });
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  width: 384px;
+  .title {
+    .back-home {
+      margin-bottom: 60px;
+    }
+    .title-text {
+      text-align: center;
+      h2 {
+        font-size: 36px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 18px;
+        color: #6b7280;
+      }
+    }
+  }
+  .account-form {
+    margin-top: 30px;
+    .footer {
+      margin-top: 40px;
+      .login-btn {
+        width: 100%;
+      }
+      .register-btn {
+        padding: 30px;
+        text-align: center;
+      }
+    }
+  }
+}
+</style>
